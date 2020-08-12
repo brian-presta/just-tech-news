a37d650519eb767ac8f9f26b8fb29cfbb025b006
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-12 16:45:54 -0700

    added hashing to passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const {Model,DataTypes} = require('sequelize')
 const sequelize = require('../config/connection')
+const bcrypt = require('bcrypt');
+
 
 class User extends Model {}
 
@@ -28,6 +30,16 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+              }
+        },
       // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
   
       // pass in our imported sequelize connection (the direct connection to our database)
