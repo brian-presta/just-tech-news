8079156aca89d64d3b5e970434ccaaf4f9b29e0b
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-13 16:59:00 -0700

    connected to jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
 const Sequelize = require("sequelize")
 require('dotenv').config()
 
-
-const sequelize = new Sequelize("just_tech_news_db",process.env.DB_USER,process.env.DB_PW, {
+if (process.env.JAWSDB_URL) {
+   const sequelize = new Sequelize(process.env.JAWSDB_URL);
+} 
+else {
+    const sequelize = new Sequelize("just_tech_news_db",process.env.DB_USER,process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-})
+})}
 
 module.exports = sequelize
