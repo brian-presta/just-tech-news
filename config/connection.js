314656f7acbc5db2ea314958052929f4cfdbe038
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-18 11:16:05 -0700

    refactored file structure for mcv

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const Sequelize = require("sequelize")
 require('dotenv').config()
 
+let sequelize
 if (process.env.JAWSDB_URL) {
-   let sequelize = new Sequelize(process.env.JAWSDB_URL);
+   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } 
 else {
-    let sequelize = new Sequelize("just_tech_news_db",process.env.DB_USER,process.env.DB_PW, {
+    sequelize = new Sequelize("just_tech_news_db",process.env.DB_USER,process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
