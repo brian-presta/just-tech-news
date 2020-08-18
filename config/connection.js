ec7cbb8ccab8766f8810b826a18faccc5ceb12a9
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-18 10:50:08 -0700

    added jawsdb functionality to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ const Sequelize = require("sequelize")
 require('dotenv').config()
 
 if (process.env.JAWSDB_URL) {
-   const sequelize = new Sequelize(process.env.JAWSDB_URL);
+   let sequelize = new Sequelize(process.env.JAWSDB_URL);
 } 
 else {
-    const sequelize = new Sequelize("just_tech_news_db",process.env.DB_USER,process.env.DB_PW, {
+    let sequelize = new Sequelize("just_tech_news_db",process.env.DB_USER,process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
