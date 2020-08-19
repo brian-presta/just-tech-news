387a0b0a27faa9e1d0109fbb84bbac97fa60cb36
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-19 14:09:46 -0700

    set up logout logic

--- controllers/api/user_routes.js
+++ controllers/api/user_routes.js
@@ -134,6 +134,17 @@ router.delete('/:id', (req, res) => {
     else {
       res.status(400).json({ message: 'Incorrect password!' });
     }
-  })
+})
+
+router.post('/logout', (req,res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy( () => {
+        res.status(204).end()
+    })
+  }
+  else {
+    res.status(404).end()
+  }
+})
 
 module.exports = router;
