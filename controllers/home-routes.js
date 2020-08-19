28e8435d4b96ee683017445023eb9f0794b0466e
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-19 14:57:12 -0700

    added single post template and controller

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,4 +47,41 @@ router.get('/login', (req,res) => {
       res.render('login');
 })
 
+router.get('/post/:id', async (req,res) => {
+    try {
+        const rawPost = await Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          ],
+          include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        });
+        const post = rawPost.get({plain: true})
+        res.render('single-post', { post })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
